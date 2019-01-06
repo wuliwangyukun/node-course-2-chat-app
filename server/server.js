b210ce9e5ba3f010711799586e84f902414a9fa3
wyk <wangyukun@haxibiao.com>
2019-01-06 19:21:56 +0800

    add connect and disconnect handler

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 //拼接路径
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.render(publicPath + 'index.html');
-// })
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('user was disconnected');
+    })
+})
+
+server.listen(port, () => {
     console.log(`server start for ${port}`);
 })
