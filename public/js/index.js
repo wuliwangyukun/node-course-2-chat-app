9a4425743fc07c47d53816c89a85aa11c08592a8
wyk <wangyukun@haxibiao.com>
2019-01-07 23:35:22 +0800

    add createMessage and newMessage event

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connected', function () {
+    console.log('connect to server');
+
+})
+
+socket.emit('createMessage', {
+    to: 'july@exeample.com',
+    text: 'hey',
+})
+
+socket.on('newMessage', function (data) {
+    console.log('data', data);
+})
+
+socket.on('disconnected', function () {
+    console.log('disconnected from server');
+})
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'michael',
+        text: 'what going on',
+        createAt: 123
+    })
+
+    socket.on('createMessage', function (data) {
+        console.log(data);
+    })
+
     socket.on('disconnect', () => {
         console.log('user was disconnected');
     })
